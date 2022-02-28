8720d6c574cc46cb668b05db1f9589f47caf6b01
HannesH <33062605+HannesGitH@users.noreply.github.com>
2022-02-28 12:36:49 +0100

    yess

--- Assets/Scripts/PlayerMaybe.cs
+++ Assets/Scripts/PlayerMaybe.cs
@@ -7,9 +7,9 @@ using UnityEngine;
 public class PlayerMaybe : MonoBehaviour
 {
 
-    [Range(0,1)] public float speed = 0.2f; 
+    [Range(1,50)] public float speed = 20f; 
 
-    public Vector3 pos;
+    // public Vector3 pos;
 
     private void Start()
     {
@@ -18,12 +18,13 @@ public class PlayerMaybe : MonoBehaviour
 
     void Update() 
     { 
-        transform.RotateAround(Vector3.zero, Vector3.up, -speed * Input.GetAxis("Horizontal"));
+        transform.RotateAround(Vector3.zero, transform.right, speed * Time.deltaTime * Input.GetAxis("Vertical"));
+        transform.RotateAround(transform.position, transform.forward, -speed * Time.deltaTime * Input.GetAxis("Horizontal"));
         //pos += speed*new Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical"));
         //transform.position = pos;
-        transform.LookAt(new Vector3(0,0,0)); 
-        transform.Translate(Vector3.Normalize(transform.position)*speed*Input.GetAxis("Mouse ScrollWheel"));
-        //// _renderer.enabled = (currentDistance > hideDistance); 
+        // transform.LookAt(new Vector3(0,0,0)); 
+        transform.position += transform.forward * -speed * Input.GetAxis("Mouse ScrollWheel");
+        //// _renderer.enabled = (currentDistance > hideDistance); w
     }
 
 }
